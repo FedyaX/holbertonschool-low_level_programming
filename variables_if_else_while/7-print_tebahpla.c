3fa7077e59645333357b2608e0ed0fd5272034c0
FedyaX <zeynalovafatime644@gmail.com>
2024-10-27 17:25:26 +0000

    7

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -7,16 +7,12 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char letter;
 
-	for (letter = 'z'; letter >= 'a'; letter--)
-    	{    
+	for (letter = 'z'; letter >= 'a'; letter--)   
 		putchar(letter);
-    	}
-		putchar('\n');
-
-	return (0);
+	putchar('\n');
+return (0);
 }
