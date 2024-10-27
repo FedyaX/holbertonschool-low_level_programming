1232ef45e29b9b83124ae90e369c6ca941d7e3dd
FedyaX <zeynalovafatime644@gmail.com>
2024-10-27 15:21:55 +0000

    2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -8,13 +8,12 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
+int main(void) 
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++) 
 		putchar(letter);
-		putchar('\n');
-
+		putchar('\n');   
 	return (0);
 }
